e8434ad031537df7cbbe02f04c1db2f25ce2353a
kjh-busan <picsmash7@gmail.com>
2023-04-09 20:35:30 +0900

    feat: step3

--- redux-demo.js
+++ redux-demo.js
@@ -1,13 +1,23 @@
 const redux = require("redux");
 
 const counterReducer = (state = { counter: 0 }, action) => {
-  return {
-    counter: state.counter + 1,
-  };
+  console.log("action : ", action.type);
+  if (action.type === "INCREASE") {
+    return {
+      counter: state.counter + 1,
+    };
+  }
+  if (action.type === "DECREASE") {
+    return {
+      counter: state.counter - 1,
+    };
+  }
+
+  return state;
 };
 
 const sotre = redux.createStore(counterReducer);
-// console.log(sotre.getState());
+console.log("sotre.getState : ", sotre.getState());
 
 const counterSubsriber = () => {
   const lastestState = sotre.getState();
@@ -17,3 +27,4 @@ const counterSubsriber = () => {
 sotre.subscribe(counterSubsriber);
 
 sotre.dispatch({ type: "INCREASE" });
+sotre.dispatch({ type: "DECREASE" });
